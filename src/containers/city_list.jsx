c565b35d93a6319dd26566d46753b074f49ce942
Juan Garcia <juagarca@gmail.com>
2020-12-28 16:53:40 +0000

    linked redux state with componen by adding mapDispacthToProps and mapStateToProps

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import City from '../containers/city';
 
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setCities } from '../actions';
+
 class CityList extends Component {
 
   componentWillMount() {
-
+    this.props.setCities();
   }
 
   render() {
@@ -20,4 +24,17 @@ class CityList extends Component {
   }
 }
 
-export default CityList;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setCities: setCities },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    cities: state.cities
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
